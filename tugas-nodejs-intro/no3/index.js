5fea3f45de50d3ab659fe63b5ff7e637669cce3a
Sander <sandervdb23@gmail.com>
2024-08-20 16:36:14 +0700

    submit no3

---
+++ tugas-nodejs-intro/no3/index.js
@@ -0,0 +1,8 @@
+// Mengimpor fungsi sum dari modul math.js
+const sum = require('./math');
+
+// Menggunakan fungsi sum untuk menjumlahkan dua angka
+const result = sum(5, 7);
+
+// Mencetak hasil ke konsol
+console.log(`The sum of 5 and 7 is: ${result}`);
---
+++ tugas-nodejs-intro/no3/math.js
@@ -0,0 +1,7 @@
+// Fungsi untuk menjumlahkan dua angka
+function sum(a, b) {
+    return a + b;
+}
+
+// Mengekspor fungsi sum sehingga bisa digunakan di file lain
+module.exports = sum;
