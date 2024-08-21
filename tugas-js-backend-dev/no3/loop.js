9dc413c58beafbc5257eb9cb58825fab148e8b2b
Sander <sandervdb23@gmail.com>
2024-08-21 15:31:35 +0700

    submit no3

---
+++ tugas-js-backend-dev/no3/loop.js
@@ -0,0 +1,7 @@
+// Membuat array berisi angka 1 sampai 10
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// Menggunakan forEach untuk mencetak setiap angka dalam array
+numbers.forEach((number) => {
+    console.log(number);
+});
